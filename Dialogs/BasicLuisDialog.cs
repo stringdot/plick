7a40fa73369962afa27acd8bf36cab1b6d263cdf
StringDot <vtxcosmyn@yahoo.com>
2017-09-28 00:34:16 +0300

    added basic intents

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -1,14 +1,15 @@
 using System;
 using System.Configuration;
 using System.Threading.Tasks;
-
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
+using System.Net;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Bot.Sample.LuisBot
 {
-    // For more information about this template visit http://aka.ms/azurebots-csharp-luis
+  
     [Serializable]
     public class BasicLuisDialog : LuisDialog<object>
     {
@@ -16,19 +17,70 @@ namespace Microsoft.Bot.Sample.LuisBot
         {
         }
 
-        [LuisIntent("None")]
-        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        [LuisIntent("Greeting")]
+        public async Task Greeting(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync($"You have reached the none intent. You said: {result.Query}"); //
+            Random rand = new Random();
+            int pick = rand.Next(1, 10);
+            if (pick <= 5)
+                await context.PostAsync("Well hello there.");
+            else await context.PostAsync("Hi. Welcome.");
             context.Wait(MessageReceived);
         }
 
-        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
-        // Finally replace "MyIntent" with the name of your newly created intent in the following handler
-        [LuisIntent("MyIntent")]
-        public async Task MyIntent(IDialogContext context, LuisResult result)
+        [LuisIntent("Thanks")]
+        public async Task Thanks(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync($"You have reached the MyIntent intent. You said: {result.Query}"); //
+            await context.PostAsync("You are welcome.");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("HowAreYou")]
+        public async Task HowAreYou(IDialogContext context, LuisResult result)
+        {
+            Random rand = new Random();
+            int pick = rand.Next(1, 10);
+            if (pick <= 5)
+                await context.PostAsync("I am fine. Thank you.");
+            else await context.PostAsync("Very well, thanks.");
+            context.Wait(MessageReceived);
+        }
+
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string responseString = string.Empty;
+
+            var query = result;
+            var builder = new UriBuilder($"https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/538f47ef-7d40-4108-ae53-f14be0a58d44/generateAnswer");
+
+            //Add the question as part of the body
+            var postBody = $"{{\"question\": \"{query}\"}}";
+
+            //Send the POST request
+            using (WebClient client = new WebClient())
+            {
+                //Set the encoding to UTF8
+                client.Encoding = System.Text.Encoding.UTF8;
+
+                //Add the subscription key header
+                client.Headers.Add("Ocp-Apim-Subscription-Key", "10022448e00340129eb24a360a779a4f");
+                client.Headers.Add("Content-Type", "application/json");
+                responseString = client.UploadString(builder.Uri, postBody);
+            }
+
+            JObject r = JObject.Parse(responseString);
+            string cheat = (string)r.SelectToken("answers[0].answer");
+            if (cheat != "No good match found in the KB")
+            {
+                await context.PostAsync(cheat);
+            }
+            else
+            {
+                await context.PostAsync("I don't have an answer for that.");
+            }
             context.Wait(MessageReceived);
         }
     }
